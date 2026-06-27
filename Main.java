fb244ceae8b11eab7e287691b8585fd1ed6d2f67
Abigael <iyalolaoluwatosin@gmail.com>
2026-06-27 17:50:27 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,32 +1,48 @@
- class Employee{
-    int Id_no;
+class Person{
     String name;
+    int id;
+
+    Person(String name,int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    void register(){
+        System.out.println("Person" + name +"with ID" + id + "is registered.");
+    }
+}
+
+class Teacher extends Person{
     float salary;
+    Teacher(String name, int id, float salary){
+        super(name,id);
+        this.salary=salary;
+    }
 
-    //Constructor Method
-    Employee(){
-        System.out.println("****************");
-        Id_no=2314;
-        name="Penguin";
-        salary=20000;
+@Override
+    void register(){
+        System.out.println("Teacher" + name +"with ID" + id + "is registered with salary💵💵" + salary);
+    }
+}class Student extends Person{
+    String grade;
+    Student(String name, int id, String grade){
+        super(name,id);
+        this.grade=grade;
+    }
+
+@Override
+    void register(){
+        System.out.println("Student" + name +"with ID" + id + "is registered with grade💯" + grade);
 
     }
-    void display(){
-        System.out.println(Id_no + " | " + name + " | " + salary);
-    }
- }
- class Main{
-    public static void main(String[] args){
-        //Object creation
-        Employee emp1=new Employee();
-        Employee emp2=new Employee();
-        Employee emp3=new Employee();
-        Employee emp4=new Employee();
+}
+public class Main {
+    public static void main(String[] args) {
+        Person p1 = new Student() ("John", 1, "B");
+        Person p2 = new Teacher ("Alice", 2, 50000.0f);
 
-        emp1.display();
-        emp2.display();
-        emp3.display();
-        emp4.display();
-          
+        p1.register();
+        p2.register();
+
     }
- }
+}
