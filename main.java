e7110aa49e3ab1d4f27458c07dcde601dfaff0db
Abigael <iyalolaoluwatosin@gmail.com>
2026-08-16 17:55:21 +0100

    Add files via upload

--- main.java
+++ main.java
@@ -1,25 +1,39 @@
-interface calculator{
-    int divide(int a, int b)throws ArithmeticException;
-}
-
-class simpleCalculator implements calculator{
-   @Override
-   public int divide(int a, int b) throws ArithmeticException {
-        if(b==0)
-            throw new ArithmeticException("Division by zero is not allowed.");
-        return a/b;
-    }
-}
+import java.util.*;
 
 public class main {
-    public static void main(String[]args){
-        calculator calc = new simpleCalculator();
-        try {
-            int result = calc.divide(10, 2);
-            System.out.println("Result: " + result);
-        } catch (ArithmeticException e) {
-            System.out.println("Error: " + e.getMessage());
+    public static void solution(ArrayList<Integer> list) {
+        for (int i = list.size() - 1; i >= 0; i--) {
+            if (isPrime(list.get(i))) {
+                list.remove(i);
+            }
         }
-        System.out.println("Program continues after exception handling.");
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number of integers you want to input: ");
+        int n = scan.nextInt();
+
+        ArrayList<Integer> al = new ArrayList<>();
+        System.out.println("Enter the " + n + " integers:");
+        for (int i = 0; i < n; i++) {
+            al.add(scan.nextInt());
+        }
+
+        solution(al);
+
+        System.out.println("Modified list (without primes): " + al);
     }
 }
