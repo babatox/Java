1900865c34f87504d3b023a5eaf4ca27bfe47bf3
Abigael <iyalolaoluwatosin@gmail.com>
2026-09-05 17:53:44 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,26 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
+    public static void Display(int[] arr,int index){
+        if (index == arr.length) {
+            return;
+        }
+        System.out.println(arr[index]);
+        Display(arr, index + 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
+        System.out.println("Enter the size of the array:");
         int n = sc.nextInt();
 
-        printDecreasing(n);
-        
+        int[] arr = new int[n];
+        System.out.println("Enter" + n + " elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print(" Array elements: ");
+        Display(arr, 0);
+        sc.close();
     }
-    public static void printDecreasing(int n){
-        if (n == 0)return;
-        System.out.println(n);
-
-        printDecreasing(n-1);
-
-
-    }
+    
 }
