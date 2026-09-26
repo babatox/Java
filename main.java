c89fdd50f5e179e22a5ef5b8811bcbf22a08f112
Abigael <iyalolaoluwatosin@gmail.com>
2026-09-26 17:56:29 +0100

    Add files via upload

--- main.java
+++ main.java
@@ -1,39 +1,41 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class main {
-    public static void solution(ArrayList<Integer> list) {
-        for (int i = list.size() - 1; i >= 0; i--) {
-            if (isPrime(list.get(i))) {
-                list.remove(i);
+class BubbleSort{
+    void bubblesort(int arr[]){
+        int n=arr.length;
+        for(int i = 0; i < n-1; i++){
+            boolean swapped=false;
+            for(int j = 0; j < n-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
             }
         }
     }
-
-    public static boolean isPrime(int n) {
-        if (n <= 1) {
-            return false;
+    void printArray(int arr[]){
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
         }
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
+        System.out.println();
     }
-
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the number of integers you want to input: ");
-        int n = scan.nextInt();
-
-        ArrayList<Integer> al = new ArrayList<>();
-        System.out.println("Enter the " + n + " integers:");
-        for (int i = 0; i < n; i++) {
-            al.add(scan.nextInt());
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the elements: ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
         }
-
-        solution(al);
-
-        System.out.println("Modified list (without primes): " + al);
+        BubbleSort ob = new BubbleSort();
+        ob.bubblesort(arr);
+        System.out.println("Sorted array: ");
+        ob.printArray(arr);
     }
-}
